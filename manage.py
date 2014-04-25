aaa239a615aa0114becb5bf1dc0e00e0315e6da8
faizanbhat <faizan.bhat@gmail.com>
2014-04-25 17:02:50 -0400

    Added dir path to sys.path so manage.py is able to read settings_local

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+os.path.realpath(os.path.dirname(__file__))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
